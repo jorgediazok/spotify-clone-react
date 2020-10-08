4dd9f21305c8992eadd4a9ca39996e2f0608d791
Jorge <jorgediazok@gmail.com>
2020-10-08 18:08:10 -0300

    grid done

--- src/Player.js
+++ src/Player.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Sidebar from './Sidebar';
+import Body from './Body';
+import Footer from './Footer';
 import './Player.css';
 
 function Player({ spotify }) {
   return (
     <div className="player">
-      <div className="player__body"></div>
+      <div className="player__body">
+        <Sidebar />
+        <Body />
+      </div>
+      <Footer />
     </div>
   );
 }
