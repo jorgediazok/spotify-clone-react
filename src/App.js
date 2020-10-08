d9a06032287f36eab1f95a5bd50a5addbb064b49
Jorge <jorgediazok@gmail.com>
2020-10-08 14:04:12 -0300

    added token

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Login from './Login';
+import Player from './Player';
 import './App.css';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = '';
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/Player.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Player.css';
+
+function Player() {
+  return (
+    <div>
+      <h1>YOU ARE LOGGED IN</h1>
+    </div>
+  );
+}
+
+export default Player;
