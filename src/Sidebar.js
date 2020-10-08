f86e635767eb37eb98af8d26f5fe999a4485dc84
Jorge <jorgediazok@gmail.com>
2020-10-08 20:26:43 -0300

    playlist dynamic

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import SidebarOption from './SidebarOption';
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
+import { useDataLayerValue } from './DataLayer';
 import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 import './Sidebar.css';
 
 function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
   return (
     <div className="sidebar">
       <img
@@ -19,13 +21,9 @@ function Sidebar() {
       <br />
       <strong className="sidebar__title">PLAYLISTS</strong>
       <hr />
-      <SidebarOption title="World Music - Gogol Bordello" Icon={null} />
-      <SidebarOption title="Rock - Muse Live Argentina" Icon={null} />
-      <SidebarOption title="Punk - The Clash / London Calling" Icon={null} />
-      <SidebarOption title="Punk - Ramones Essentials" Icon={null} />
-      <SidebarOption title="Russian Rock - Leningrad" Icon={null} />
-      <SidebarOption title="Movies Music - Goodbye Lenin OST" Icon={null} />
-      <SidebarOption title="Anime Music - Death Note OST" Icon={null} />
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption title={playlist.name} />
+      ))}
     </div>
   );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -9,16 +9,24 @@ export const initialState = {
 
 const reducer = (state, action) => {
   console.log(action);
+
   switch (action.type) {
     case 'SET_USER':
       return {
         ...state,
         user: action.user,
       };
-    case 'SET_TOKEN': {
+
+    case 'SET_TOKEN':
       return {
         ...state,
         token: action.token,
+      };
+
+    case 'SET_PLAYLISTS': {
+      return {
+        ...state,
+        playlists: action.playlists,
       };
     }
     default:
