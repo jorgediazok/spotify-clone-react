81c1188a76871ea25e814b14f2ff2914339235ad
Jorge <jorgediazok@gmail.com>
2020-10-09 01:28:51 -0300

    adding styles to the footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import { Grid, Slider } from '@material-ui/core';
+
 import './Footer.css';
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>I am the footer</h1>
+      <div className="footer__left">
+        <h1>Album and song details</h1>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__icon" />
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
